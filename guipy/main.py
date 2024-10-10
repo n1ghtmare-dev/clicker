ad5fc4622fc666bd5a4a55afd4482584dd4decbb
nightmare-dev <oboev3691@gmail.com>
2024-10-11 01:21:10 +0300

    Update the structure of project a bit, also add the db.py and db.db, click works with a db

--- guipy/main.py
+++ guipy/main.py
@@ -1,28 +1,40 @@
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import Qt
 from ui_main import Ui_MainWindow
+from database import Database
+import asyncio
 import sys
 
+db = Database('database.db')
 
 class MinerWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MinerWindow, self).__init__()
         self.setupUi(self)
+
+
         # Объявляем переменные с нормальными названиями
         self.click_btn = self.pushButton
         self.clicks_counter = self.label_2
+        self.rang_bar = self.progressBar
 
         # Объявляем события
         self.click_btn.clicked.connect(self.click)
 
+    # Обновляем переменные
+    async def data_update(self):
+        self.one_click = await db.get_click()
+        print(self.one_click)
+
     def click(self):
-        one_click = 1
-        self.clicks_counter.setText(str(int(self.clicks_counter.text())+one_click))
+        click = int(self.clicks_counter.text())+self.one_click
+        self.clicks_counter.setText(str(click))
 
 
 app = QApplication(sys.argv)
 
 window = MinerWindow()
+asyncio.run(window.data_update())
 window.show()
 
 app.exec()
