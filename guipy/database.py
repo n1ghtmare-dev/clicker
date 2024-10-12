028a1b205a71f5550a4155b9d6c435df18c57528
nightmare-dev <oboev3691@gmail.com>
2024-10-13 01:56:58 +0300

    added assets, ranks system, new GUI

--- guipy/database.py
+++ guipy/database.py
@@ -37,6 +37,31 @@ class Database:
             finally:
                 lock.release()
 
+    def edit_level(self, level):
+        with self.connection:
+            try:
+                lock.acquire(True)
+                sql = "UPDATE user SET level = ?"
+                self.cursor.execute(sql, (level,))
+            finally:
+                lock.release()
+
+    def edit_user(self, one_click=None, clicks_counter=None, level=None):
+        var = []
+        if one_click:
+            var.append(f"one_click = '{one_click}'")
+        if clicks_counter:
+            var.append(f"clicks_counter = '{clicks_counter}'")
+        if level:
+            var.append(f"level = '{level}'")
+        with self.connection:
+            try:
+                lock.acquire(True)
+                sql = f"UPDATE user SET {','.join(var)}"
+                self.cursor.execute(sql)
+            finally:
+                lock.release()
+
     def save_user_data(self, one_click, clicks_counter):
         with self.connection:
             try:
--- guipy/main.py
+++ guipy/main.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import QThreadPool
+from PySide6.QtGui import QPixmap
 from ui_main import Ui_MainWindow
 from asyncio import create_task
 from database import Database
@@ -17,13 +18,6 @@ class MinerWindow(QMainWindow, Ui_MainWindow):
         super(MinerWindow, self).__init__()
         self.setupUi(self)
 
-        # Объявляем переменные с нормальными названиями
-        self.click_btn = self.pushButton
-        self.clicks_counter = self.label_2
-        self.rang_bar = self.progressBar
-        self.rating = self.label_6
-        self.goal = self.label_4
-
         # Объявляем переменные, которые обновятся
         self.one_click = 1
 
@@ -35,7 +29,8 @@ class MinerWindow(QMainWindow, Ui_MainWindow):
 
         # Объявляем события
         self.click_btn.clicked.connect(self.click)
-        self.pushButton_3.clicked.connect(self.close)
+        self.level_upgrade_btn.clicked.connect(self.level_upgrade)
+
 
 
     # Обновляем переменные
@@ -49,12 +44,21 @@ class MinerWindow(QMainWindow, Ui_MainWindow):
         level_info = db.get_level_info(user_info[2])
         self.goal.setText(str(level_info[2]))
         self.rating.setText(self.clicks_counter.text())
-        self.rang_bar.setValue(round((int(self.clicks_counter.text()) / int(self.goal.text()) * 100)))
+        self.rank_bar.setValue(round((int(self.clicks_counter.text()) / int(self.goal.text()) * 100)))
+        self.rank_img.setPixmap(QPixmap(u":/images/ranks/Shield/{}".format(level_info[3])))
 
     def click(self):
         click = int(self.clicks_counter.text())+self.one_click
         self.clicks_counter.setText(str(click))
         self.update_rang()
+
+    def level_upgrade(self):
+        user_info = db.get_user_info()
+        level_info = db.get_level_info(user_info[2])
+        if int(self.clicks_counter.text()) >= level_info[2]:
+            db.edit_user(clicks_counter=int(self.clicks_counter.text())-level_info[2], level=int(user_info[2])+1)
+            self.data_update()
+
 
     def auto_save(self):
         print("Auto save ON | 10s")
@@ -68,8 +72,8 @@ class MinerWindow(QMainWindow, Ui_MainWindow):
         self.rating.setText(cc)
         # Изменяем прогрессбар
         new_percent = round((int(cc) / int(self.goal.text()) * 100))
-        if new_percent - self.rang_bar.value() >= 1:
-            self.rang_bar.setValue(new_percent)
+        if new_percent - self.rank_bar.value() >= 1:
+            self.rank_bar.setValue(new_percent)
 
         if int(self.clicks_counter.text()) >= int(self.goal.text()):
             # Загорается кнопка и можно улучшить уровень
